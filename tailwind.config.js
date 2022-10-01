ad6dd52a86aa79d13c5af31a0375fd4a5f7ebe37
alondramora <mora_ac@yahoo.com>
2022-10-01 16:40:53 -0700

    added tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './views/**/*.ejs'
+  ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {},
   },
   plugins: [],
