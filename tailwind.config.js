7283c541fd99fcc17114a5d2f3e555c3e297069b
alondramora <mora_ac@yahoo.com>
2022-10-03 19:08:07 -0700

    more styling on home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
       lg: '976px',
       xl: '1440px'
     },
-    extend: {},
+    extend: {
+      colors: {
+        lightishRed: '#EFAEA0'
+      }
+    },
   },
   plugins: [],
 }
